b533c88fb01856fbeba28d83432e19959fc252c2
iryna.ivanova-at-669981400155 <avonavis@gmail.com>
2023-12-03 20:43:05 +0200

    fifth homework

--- fifth_lesson.py
+++ fifth_lesson.py
@@ -1,14 +1,14 @@
 import requests
 
-url_carts = 'https://dummyjson.com/carts'
+url_carts = "https://dummyjson.com/carts"
 
 response = requests.get(url=url_carts)
 data_from_web = response.json()
-carts = data_from_web['carts']
+carts = data_from_web["carts"]
 
 for cart in carts:
-    if cart['userId'] == 56:
-        products = cart['products']
+    if cart["userId"] == 56:
+        products = cart["products"]
         for product in products:
-            if product['discountPercentage'] > 15:
-                print(product['title'])
+            if product["discountPercentage"] > 15:
+                print(product["title"])
