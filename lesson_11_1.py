13a51b1e71d835cd144071cd9d3db4ae0063eed6
iryna.ivanova-at-669981400155 <avonavis@gmail.com>
2023-12-26 17:28:25 +0200

    Lesson 11 - Decorators - pt.1

--- lesson_11_1.py
+++ lesson_11_1.py
@@ -6,7 +6,7 @@ from typing import Callable
 def log_to_file(func: Callable):
     def wrapper(*args, **kwargs):
         filename = func.__name__
-        log_entry = f"Method {filename} was called with args {args} at {time.strftime('%Y-%m-%d %H:%M:%S')}\n"
+        log_entry = f"Method {filename} was called with args {args, kwargs} at {time.strftime('%Y-%m-%d %H:%M:%S')}\n"
         result = func(*args, **kwargs)
         log_entry += f"Result: {result}\n"
         with open(f'data/{filename}.txt', 'a') as file:
@@ -18,17 +18,19 @@ def log_to_file(func: Callable):
 
 
 @log_to_file
-def add(x, y):
+def add(x: int, y: int):
     return x + y
 
 
 @log_to_file
-def multiply(x, y):
+def multiply(x: int, y: int):
     return x * y
 
 
 random_x = random.randint(0, 9)
 random_y = random.randint(0, 9)
 
-result_add = add(random_x, random_y)
-result_multiply = multiply(random_x, random_y)
+add(random_x, random_y)
+multiply(random_x, random_y)
+multiply(x=random_x, y=random_y)
+multiply(random_x, y=random_y)
